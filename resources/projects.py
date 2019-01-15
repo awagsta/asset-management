7b6924ecaab0d42f8592531da7be22b4c04df20a
Alexander Wagstaff <awagstaff6@gmail.com>
2019-01-15 14:53:23 -0600

    Added more project classes.

--- resources/projects.py
+++ resources/projects.py
@@ -13,4 +13,24 @@ class Project(Resource):
             project = gl.projects.get(id)
             return jsonify({"project": project.attributes})
 
+class AllProjects(Resource):
+    # Get all existing projects.
+    def get(self, token):
+        with gitlab.Gitlab(repo_url, ssl_verify=False, private_token=token) as gl:
+            projects = gl.projects.get()
+            projectList = []
+            for project in projects:
+                projectList.append({"project": project.attributes})
+        return jsonify({"Projects": projectList})
 
+class Project_List(Resource):
+    #Get all Projects for a user by user_id.
+
+    def get(self, id, token):
+        with gitlab.Gitlab(repo_url, ssl_verify=False, private_token=token) as gl:
+            projects = gl.projects.get(id)
+            projectList = []
+            for project in projects:
+                projectList.append({"project": project.attributes})
+        return jsonify({"Projects": projectList})
+        
